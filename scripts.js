6088c976d836c9d3de3cc7b8a3ad6d3f517af6fa
Mateus <myneuzyn@gmail.com>
2025-06-10 23:21:16 -0300

    Scripts.js adicionado

---
+++ scripts.js
@@ -0,0 +1,26 @@
+﻿// script.js
+
+const select1 = document.getElementById('select-d087');
+const select2 = document.getElementById('select-4333');
+
+function updateOptions(changedSelect, otherSelect) {
+  const selectedValue = changedSelect.value;
+
+  // Reabilita todas as opções do outro select
+  Array.from(otherSelect.options).forEach(option => {
+    option.disabled = false;
+  });
+
+  // Desabilita a opção selecionada no outro select
+  if (selectedValue) {
+    const optionToDisable = Array.from(otherSelect.options).find(
+      option => option.value === selectedValue
+    );
+    if (optionToDisable) {
+      optionToDisable.disabled = true;
+    }
+  }
+}
+
+select1.addEventListener('change', () => updateOptions(select1, select2));
+select2.addEventListener('change', () => updateOptions(select2, select1));
